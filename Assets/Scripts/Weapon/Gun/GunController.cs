43a86dca0334aac6438582bcf670a41bebd2cc77
Kulyka <max837352@gmail.com>
2025-08-21 18:06:57 +0300

    Gun Fix

--- Assets/Scripts/Weapon/Gun/GunController.cs
+++ Assets/Scripts/Weapon/Gun/GunController.cs
@@ -1,44 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+// Гарантуємо, що цей скрипт виконається ПІСЛЯ більшості контролерів руху
+[DefaultExecutionOrder(100)]
 public class GunController : MonoBehaviour
 {
 	[SerializeField] private Animator gunAnim;
 	[SerializeField] private Transform gun;
 	[SerializeField] private float gunDistance = 1.5f;
-	private bool gunFacingRight = true;
-	private void Update()
+
+	// (Необов'язково) Якщо хочеш, щоб зброя брала позицію не з this.transform, а з окремої точки (наприклад "HandPivot")
+	[SerializeField] private Transform aimOrigin;
+
+	private Vector3 _initialGunLocalScale;
+
+	private Transform Origin => aimOrigin != null ? aimOrigin : transform;
+
+	private void Awake()
 	{
+		if (gun == null)
+		{
+			Debug.LogError("[GunController] 'gun' не призначено.");
+			enabled = false;
+			return;
+		}
+		_initialGunLocalScale = gun.localScale;
+	}
+
+	private void LateUpdate()
+	{
+		// 1) Мишу в світ
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		Vector3 direction = mousePos - transform.position;
-		gun.rotation = Quaternion.Euler(new Vector3(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg));
+		mousePos.z = 0;
 
-		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-		gun.position = transform.position + Quaternion.Euler(0, 0, angle) * new Vector3(gunDistance, 0, 0);
+		// 2) Нормалізований напрямок
+		Vector3 dir = (mousePos - Origin.position);
+		if (dir.sqrMagnitude < 0.000001f) return;
+		dir.Normalize();
 
+		// 3) Компенсація дзеркалення від батьківського масштабу.
+		// Якщо Root фліпиться через localScale.x *= -1, то дитина дзеркалиться.
+		// Робимо і у зброї localScale.x з тим самим знаком, щоб у підсумку ГЛОБАЛЬНА орієнтація була нормальна.
+		float parentSignX = Mathf.Sign(Origin.lossyScale.x); // < 0, якщо гравець "задзеркалений"
+		gun.localScale = new Vector3(
+			Mathf.Abs(_initialGunLocalScale.x) * (parentSignX < 0 ? -1f : 1f),
+			Mathf.Abs(_initialGunLocalScale.y),
+			_initialGunLocalScale.z
+		);
+
+		// 4) Поворот по куту до миші
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		gun.rotation = Quaternion.Euler(0f, 0f, angle);
+
+		// 5) Позиція на відстані від "Origin"
+		gun.position = Origin.position + dir * gunDistance;
+
+		// 6) Постріл
 		if (Input.GetButtonDown("Shoot"))
 			Shoot();
-
-		GunFlipController(mousePos);
-	}
-
-	private void GunFlipController(Vector3 mousePos)
-	{
-		if (mousePos.x < gun.position.x && gunFacingRight)
-			GunFlip();
-		else if (mousePos.x > gun.position.x && !gunFacingRight)
-			GunFlip();
-	}
-
-	private void GunFlip()
-	{
-		gunFacingRight = !gunFacingRight;
-		gun.localScale = new Vector3(gun.localScale.x, gun.localScale.y * -1, gun.localScale.z);
 	}
 
 	private void Shoot()
 	{
-		gunAnim.SetTrigger("Shoot");
+		if (gunAnim != null)
+			gunAnim.SetTrigger("Shoot");
 	}
 }
