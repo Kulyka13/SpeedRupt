2b38d7fcfd926a134b6863d8d4f07c6fedd27677
Kulyka <max837352@gmail.com>
2025-09-07 19:05:44 +0300

    Bullet mini update

--- Assets/Scripts/Weapon/Gun/Bullet.cs
+++ Assets/Scripts/Weapon/Gun/Bullet.cs
@@ -6,7 +6,7 @@ public class Bullet : MonoBehaviour
 {
 	private Rigidbody2D rb => GetComponent<Rigidbody2D>();
 	private void Update() => transform.right = rb.velocity;
-
+	/*
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if(collision.tag == "Target")
@@ -14,5 +14,10 @@ public class Bullet : MonoBehaviour
 			Destroy(gameObject);
 			Destroy(collision.gameObject);
 		}
+	}
+	*/
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Destroy(gameObject);
 	}
 }
