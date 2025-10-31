d8500771cef2063542e3606adb9f18192da66927
Kulyka <max837352@gmail.com>
2025-10-31 12:41:36 +0200

    WaySpawner and small fix wall enemy

---
+++ Assets/Scripts/Enemies/WaveSpawner.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+[System.Serializable]
+public class Wave
+{
+	public string waveName;
+	public int noOfEnemies;
+	public GameObject[] typeOfEnemies;
+	public float spawnInterval;
+}
+
+public class WaveSpawner : MonoBehaviour
+{
+	[SerializeField] private Wave[] waves;
+	[SerializeField] private Transform[] spawnPoints;
+
+	private Wave currentWave;
+	private int currentWaveNumber;
+	private bool canSpawn = true;
+	private float nextNextSpawnTime;
+
+	private void Update()
+	{
+		currentWave = waves[currentWaveNumber];
+		SpawnWave();
+		GameObject[] totalEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+		if(totalEnemies.Length == 0 && !canSpawn && currentWaveNumber+1 != waves.Length)
+		{
+			currentWaveNumber++;
+			canSpawn = true;
+		}
+	}
+	private void SpawnWave()
+	{
+		if (canSpawn && nextNextSpawnTime < Time.time)
+		{
+			GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
+			Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+			Instantiate(randomEnemy, randomPoint.position, Quaternion.identity);
+			currentWave.noOfEnemies--; 
+			nextNextSpawnTime = Time.time + currentWave.spawnInterval;
+			if(currentWave.noOfEnemies == 0)
+			{
+				canSpawn = false;
+			}
+		}
+	}
+}
