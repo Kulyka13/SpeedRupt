a1fc631dc13375b6bd7bbc0cf1d42459c40a6659
Kulyka <max837352@gmail.com>
2025-11-18 19:42:29 +0200

    Boss Flipping

--- Assets/Scripts/Enemies/Boss/BossMove.cs
+++ Assets/Scripts/Enemies/Boss/BossMove.cs
@@ -7,14 +7,19 @@ public class BossMove : StateMachineBehaviour
     [SerializeField] private float speed;
     private Transform player;
     private Rigidbody2D rb;
+    private BossFlipping boss;
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponent<Rigidbody2D>();
-    }
+        boss = animator.GetComponent<BossFlipping>();
+
+	}
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        boss.LookAtPlayer();
+
         Vector2 target = new Vector2(player.position.x, rb.position.y);
         Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed*Time.deltaTime);
         rb.MovePosition(newPos);
