1431f515a8b7fbb7d2a057cafea2a4de4da20517
Kulyka <max837352@gmail.com>
2025-11-18 19:17:34 +0200

    BossMove

---
+++ Assets/Scripts/Enemies/Boss/BossMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMove : StateMachineBehaviour
+{
+    [SerializeField] private float speed;
+    private Transform player;
+    private Rigidbody2D rb;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed*Time.deltaTime);
+        rb.MovePosition(newPos);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
