8643812a5811db7d57242305fd4b1438dffe0fca
Kulyka <max837352@gmail.com>
2025-11-20 20:32:48 +0200

    CutScene not working

---
+++ Assets/Scripts/Enemies/Boss/BossTimelineReceiver.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+public class BossTimelineReceiver : MonoBehaviour
+{
+    [SerializeField] private MonoBehaviour[] aiScripts;
+    private Animator bossAnimator;
+    private void Start()
+    {
+        bossAnimator = GetComponent<Animator>();
+    }
+    public void DisableAI()
+    {
+        //bossAnimator.enabled = false;
+        foreach (var script in aiScripts)
+            script.enabled = false;
+    }
+    public void EnableAI()
+    {
+        //bossAnimator.enabled = true;
+        foreach (var script in aiScripts)
+            script.enabled = true;
+    }
+}
--- Assets/Scripts/Enemies/WaveSpawner.cs
+++ Assets/Scripts/Enemies/WaveSpawner.cs
@@ -1,53 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Policy;
 using UnityEngine;
+
 [System.Serializable]
 public class Wave
 {
-	public string waveName;
-	public int numberOfEnemies;
-	public GameObject[] typeOfEnemies;
-	public GameObject[] heals;
-	public float spawnInterval;
+    public string waveName;
+    public int numberOfEnemies;
+    public GameObject[] typeOfEnemies;
+    public GameObject[] heals;
+    public float spawnInterval;
 }
 
 public class WaveSpawner : MonoBehaviour
 {
-	[SerializeField] private Wave[] waves;
-	[SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private Wave[] waves;
+    [SerializeField] private Transform[] spawnPoints;
 
-	private Wave currentWave;
-	private int currentWaveNumber;
-	private bool canSpawn = true;
-	private float nextSpawnTime;
+    [Header("Final Event Settings")]
+    [SerializeField] private GameObject[] objectsToDisable;
+    [SerializeField] private GameObject[] objectsToEnable;
 
-	private void Update()
-	{
-		currentWave = waves[currentWaveNumber];
-		SpawnWave();
-		GameObject[] totalEnemies = GameObject.FindGameObjectsWithTag("Enemy");
-		if (totalEnemies.Length == 0 && !canSpawn && currentWaveNumber + 1 != waves.Length)
-		{
-			currentWaveNumber++;
-			canSpawn = true;
-		}
-	}
-	private void SpawnWave()
-	{
-		if (canSpawn && nextSpawnTime < Time.time)
-		{
-			GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
-			GameObject randomHeal = currentWave.heals[Random.Range(0, currentWave.heals.Length)];
+    private Wave currentWave;
+    private int currentWaveNumber;
+    private bool canSpawn = true;
+    private float nextSpawnTime;
+
+    private void Update()
+    {
+        currentWave = waves[currentWaveNumber];
+        SpawnWave();
+
+        GameObject[] totalEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if (totalEnemies.Length == 0 && !canSpawn)
+        {
+            if (currentWaveNumber + 1 < waves.Length)
+            {
+                currentWaveNumber++;
+                canSpawn = true;
+            }
+            else
+            {
+                TriggerFinalEvent();
+            }
+        }
+    }
+
+    private void SpawnWave()
+    {
+        if (canSpawn && nextSpawnTime < Time.time)
+        {
             Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
-			Instantiate(randomEnemy, randomPoint.position, Quaternion.identity);
+
+            GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
+            Instantiate(randomEnemy, randomPoint.position, Quaternion.identity);
+
             currentWave.numberOfEnemies--;
-			nextSpawnTime = Time.time + currentWave.spawnInterval;
-			if (currentWave.numberOfEnemies == 0)
-			{
-				Instantiate(randomHeal, randomPoint.position, Quaternion.identity);
-				canSpawn = false;
-			}
-		}
-	}
+            nextSpawnTime = Time.time + currentWave.spawnInterval;
+
+            if (currentWave.numberOfEnemies == 0)
+            {
+                GameObject randomHeal = currentWave.heals[Random.Range(0, currentWave.heals.Length)];
+                Instantiate(randomHeal, randomPoint.position, Quaternion.identity);
+
+                canSpawn = false;
+            }
+        }
+    }
+
+    private void TriggerFinalEvent()
+    {
+        foreach (var obj in objectsToEnable)
+            if (obj != null) obj.SetActive(true);
+
+        foreach (var obj in objectsToDisable)
+            if (obj != null) obj.SetActive(false);
+
+        enabled = false;
+    }
 }
